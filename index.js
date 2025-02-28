67682dcf061f293af5cbf9bc8d099f47846961cb
wesleynjerenga <wesleynjerenga9@gmail.com>
2025-02-28 11:34:49 +0300

    function challenge

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string){
+   return string.toUpperCase(); 
+}
+function whisper(string){
+   return string.toLowerCase();
+   console.log(string);
+   }
+function logShout(string){
+   console.log(string.toUpperCase());
+}
+function logWhisper(string){
+   console.log(string.toLowerCase());
+}
+function sayHiToHeadphonedRoommate(string){
+   if (string.toUpperCase() === string){
+     return "YES INDEED!";
+   } else if (string.toLowerCase() === string){
+     return "I can't hear you!";
+   } else if (string === "Let's have dinner together!"){
+      return"I would love to!";
+}
+}
